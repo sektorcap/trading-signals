ef9fe3f60ed045801db1764f8f47bd13e992f294
Ettore Elio Caprella <ettoreelio.caprella@telecomitalia.it>
2018-05-23 23:54:30 +0200

    infinite loop with sleep

--- bin/ccts.rb
+++ bin/ccts.rb
@@ -18,15 +18,18 @@ trap_kill
 
 options = OptionParser.parse(ARGV)
 
-# begin
-  options.stocks_files.each do |f|
-    logger.info "Analyzing #{f}..."
-    ret = analyze f
-    puts ret.inspect
+while true
+  begin
+    options.stocks_files.each do |f|
+      logger.info "Analyzing #{f}..."
+      ret = analyze f
+      puts ret.inspect
+    end
+  rescue Exception => e
+     logger.error e.inspect
   end
-# rescue Exception => e
-#   logger.error e.inspect
-# end
+  sleep(60*60)
+end
 
 logger.info 'Finish'
 
